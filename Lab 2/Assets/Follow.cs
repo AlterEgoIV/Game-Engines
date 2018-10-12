5a17764f439656897f011d25d650fb17e814d03b
Carl Kavanagh <alterego01010@gmail.com>
2018-10-12 17:54:15 +0100

    Added Lab 2

---
+++ Lab 2/Assets/Follow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour {
+
+    public GameObject target;
+    Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        offset = new Vector3(0, 1, -3);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = target.transform.position + offset;
+        transform.LookAt(target.transform);
+        //transform.rotation = target.transform.rotation;
+    }
+}
---
+++ Lab 2/Assets/FollowWaypoints.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowWaypoints : MonoBehaviour {
+
+    public int numWaypoints, current;
+    public float radius, angle, speed;
+    List<Vector3> points;
+
+	// Use this for initialization
+	void Start () {
+        numWaypoints = 6;
+        current = 0;
+        radius = 6f;
+        angle = 0f;
+        speed = 10f;
+        points = new List<Vector3>();
+
+        for (int i = 0; i < numWaypoints; ++i)
+        {
+            float x = transform.position.x + Mathf.Cos(angle) * radius;
+            float z = transform.position.z + Mathf.Sin(angle) * radius;
+            points.Add(new Vector3(x, 0.5f, z));
+            angle += Mathf.PI * 2 / numWaypoints;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.forward = points[current] - transform.position;
+        transform.position = Vector3.MoveTowards(transform.position, points[current], speed * Time.deltaTime);
+
+        if(Vector3.Distance(transform.position, points[current]) < 1f)
+        {
+            current = (current + 1) % numWaypoints;
+        }
+	}
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+
+        for (int i = 0; i < points.Count; ++i)
+        {
+            Gizmos.DrawWireSphere(points[i], 1);
+        }
+    }
+}
